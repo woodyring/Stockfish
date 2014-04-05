72e6926b3bce8ebe81dd42e630ce1467729ab683
woodyring <woodyring123@gmail.com>
2014-04-05 12:16:28 +0900

    merged c86eee391830c9c74bc11be9c537ccbbf6cb412e, need any conditons

--- src/search.cpp
+++ src/search.cpp
@@ -1910,9 +1910,14 @@ moves_loop: // When in check and at SpNode search starts from here
     // The piece is the same or second's destination was vacated by the first move
     // We exclude the trivial case where a sliding piece does in two moves what
     // it could do in one move: eg. Ra1a2, Ra2a3.
+#ifdef GPSFISH
+    if (m1to == m2from || m2to == m1from) // XXX : should be add any conditions
+        return true;
+#else
     if (    m2to == m1from
         || (m1to == m2from && !squares_aligned(m1from, m2from, m2to)))
         return true;
+#endif
 
     // Second one moves through the square vacated by first one
 #ifdef GPSFISH
